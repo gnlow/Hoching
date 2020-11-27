d7af098e5af477a9f678c7ba35bcc8f5d575a7a9
Gnlow <entrydark@gmail.com>
2020-11-27 23:43:40 +0900

    simpler object

--- names.ts
+++ names.ts
@@ -39,8 +39,7 @@ type Translation = {
 type Names = {
     [P in BaseName]?: {
         name: Translation,
-        nodes?: Names,
-    }
+    } & Names
 }
 
 export const names: Names = {
@@ -49,72 +48,66 @@ export const names: Names = {
             [Lang.ko]: "아빠",
             [Lang.en]: "father",
         },
-        nodes: {
+        [Dad]: {
+            name: {
+                [Lang.ko]: "할아버지",
+                [Lang.en]: "grandfather",
+            },
             [Dad]: {
                 name: {
-                    [Lang.ko]: "할아버지",
-                    [Lang.en]: "grandfather",
-                },
-                nodes: {
-                    [Dad]: {
-                        name: {
-                            [Lang.ko]: "증조할아버지",
-                            [Lang.en]: "great-grandfather",
-                        }
-                    },
-                    [Mom]: {
-                        name: {
-                            [Lang.ko]: "증조할머니",
-                            [Lang.en]: "great-grandmother",
-                        }
-                    },
+                    [Lang.ko]: "증조할아버지",
+                    [Lang.en]: "great-grandfather",
                 }
             },
             [Mom]: {
                 name: {
-                    [Lang.ko]: "할머니",
-                    [Lang.en]: "grandmother",
+                    [Lang.ko]: "증조할머니",
+                    [Lang.en]: "great-grandmother",
                 }
             },
-            [Sibling]: {
-                name: {
-                    [Lang.ko]: {
-                        default: "삼촌/고모",
-                        m: {
-                            default: "삼촌",
-                            married: {
-                                older: "큰아빠",
-                                younger: "작은아빠",
-                            },
-                        },
-                        f: {
-                            default: "고모",
+        },
+        [Mom]: {
+            name: {
+                [Lang.ko]: "할머니",
+                [Lang.en]: "grandmother",
+            }
+        },
+        [Sibling]: {
+            name: {
+                [Lang.ko]: {
+                    default: "삼촌/고모",
+                    m: {
+                        default: "삼촌",
+                        married: {
+                            older: "큰아빠",
+                            younger: "작은아빠",
                         },
                     },
-                    [Lang.en]: "uncle",
+                    f: {
+                        default: "고모",
+                    },
                 },
-                nodes: {
-                    [Child]: {
-                        name: {
-                            [Lang.ko]: {
-                                default: "사촌",
-                                older: {
-                                    m: {
-                                        forM: "사촌형",
-                                        forF: "사촌오빠",
-                                    },
-                                    f: {
-                                        forM: "사촌누나",
-                                        forF: "사촌언니",
-                                    },
-                                },
-                                younger: "사촌동생",
-                            },
-                            [Lang.en]: "cousin",
-                        }
-                    }
-                }
+                [Lang.en]: "uncle",
             },
-        }
+            [Child]: {
+                name: {
+                    [Lang.ko]: {
+                        default: "사촌",
+                        older: {
+                            m: {
+                                forM: "사촌형",
+                                forF: "사촌오빠",
+                            },
+                            f: {
+                                forM: "사촌누나",
+                                forF: "사촌언니",
+                            },
+                        },
+                        younger: "사촌동생",
+                    },
+                    [Lang.en]: "cousin",
+                }
+            }
+        },
     },
 }
