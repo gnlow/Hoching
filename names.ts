9f2a6797b2f81507d66529000b1cbe88de27269e
Gnlow <entrydark@gmail.com>
2020-11-27 23:53:44 +0900

    simpler object (2)

--- names.ts
+++ names.ts
@@ -40,76 +40,63 @@ type Translation = {
 
 
 type Names = {
-    [P in BaseName]?: {
-        name: Translation,
-    } & Names
+    [P in BaseName]?: Translation & Names
 }
 
 export const names: Names = {
     [Dad]: {
-        name: {
-            [KO]: "아빠",
-            [EN]: "father",
-        },
+        [KO]: "아빠",
+        [EN]: "father",
+
         [Dad]: {
-            name: {
-                [KO]: "할아버지",
-                [EN]: "grandfather",
-            },
+            [KO]: "할아버지",
+            [EN]: "grandfather",
+
             [Dad]: {
-                name: {
-                    [KO]: "증조할아버지",
-                    [EN]: "great-grandfather",
-                }
+                [KO]: "증조할아버지",
+                [EN]: "great-grandfather",
             },
             [Mom]: {
-                name: {
-                    [KO]: "증조할머니",
-                    [EN]: "great-grandmother",
-                }
+                [KO]: "증조할머니",
+                [EN]: "great-grandmother",
             },
         },
         [Mom]: {
-            name: {
-                [KO]: "할머니",
-                [EN]: "grandmother",
-            }
+            [KO]: "할머니",
+            [EN]: "grandmother",
         },
         [Sibling]: {
-            name: {
-                [KO]: {
-                    default: "삼촌/고모",
-                    m: {
-                        default: "삼촌",
-                        married: {
-                            older: "큰아빠",
-                            younger: "작은아빠",
-                        },
-                    },
-                    f: {
-                        default: "고모",
+            [KO]: {
+                default: "삼촌/고모",
+                m: {
+                    default: "삼촌",
+                    married: {
+                        older: "큰아빠",
+                        younger: "작은아빠",
                     },
                 },
-                [EN]: "uncle",
+                f: {
+                    default: "고모",
+                },
             },
+            [EN]: "uncle",
+
             [Child]: {
-                name: {
-                    [KO]: {
-                        default: "사촌",
-                        older: {
-                            m: {
-                                forM: "사촌형",
-                                forF: "사촌오빠",
-                            },
-                            f: {
-                                forM: "사촌누나",
-                                forF: "사촌언니",
-                            },
+                [KO]: {
+                    default: "사촌",
+                    older: {
+                        m: {
+                            forM: "사촌형",
+                            forF: "사촌오빠",
                         },
-                        younger: "사촌동생",
+                        f: {
+                            forM: "사촌누나",
+                            forF: "사촌언니",
+                        },
                     },
-                    [EN]: "cousin",
-                }
+                    younger: "사촌동생",
+                },
+                [EN]: "cousin",
             }
         },
     },
